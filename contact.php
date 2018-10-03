c109a7ccad227600a93e308079ad4a33bc54ffff
baiyantm <baiyante.mehdi@gmail.com>
2018-10-03 13:28:32 +0200

    QR

--- contact.php
+++ contact.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="mystyle.css">
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
           crossorigin="anonymous">
 
-
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
 
     <title>HEP Campus Grenoble</title>
 </head>
--- le-quizz.php
+++ le-quizz.php
@@ -18,3 +18,41 @@
 <?php
 include ('navbar.html')
 ?>
+
+<div id="container-form" class="text-center">
+    <div class="epsititlebg">
+        <h2 id="question">Bienvenue à L'EPSI</h2>
+    </div>
+    <form>
+        <div class="radio">
+            <input type="radio" name="optradio" checked><label id="rep1">opt1</label>
+        </div>
+        <div class="radio">
+            <input type="radio" name="optradio"><label id="rep2">opt2</label>
+        </div>
+        <div class="radio">
+            <input type="radio" name="optradio"><label id="rep3">opt3</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+<script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+<script>
+    $.getJSON('Q-R.json', function(data) {
+        console.log(data);
+        $("#question").text(data.question1["text"]);
+        $("#rep1").text(data.question1["r1"]);
+        $("#rep2").text(data.question1["r2"]);
+        $("#rep3").text(data.question1["r3"]);
+    });
+</script>
+
+
