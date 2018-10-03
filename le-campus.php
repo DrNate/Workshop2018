5a1b039393e83ce8c4e56494842f29b68c593912
baiyantm <baiyante.mehdi@gmail.com>
2018-10-03 10:36:15 +0200

    images fix

--- le-campus.php
+++ le-campus.php
@@ -5,7 +5,7 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
     crossorigin="anonymous">
-  <link rel="icon" type="image/png" href="Images/logo.PNG" />
+  <link rel="icon" type="image/png" href="images/logo.PNG" />
   <link rel="stylesheet" type="text/css" href="mystyle.css">
   <!-- Required meta tags -->
   <meta charset="utf-8">
--- le-labo.php
+++ le-labo.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>EPSI</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="images/logo.PNG" />
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
           crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="mystyle.css">
 
+    <title>EPSI | Campus Grenoble</title>
 </head>
+
 <body>
 <?php
 include ('navbar.html')
